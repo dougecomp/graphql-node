eb61798672cb38714cac93025afd7e61f23288cd
Douglas Silva <douglaseusilva@gmail.com>
2020-05-11 13:28:20 -0300

    Colocando update

--- src/resolvers.js
+++ src/resolvers.js
@@ -2,7 +2,8 @@ const users = [
     {id:1,name:"Douglas",email:"douglas@douglas.com.br"},
     {id:2,name:"Carlos",email:"carlos@carlos.com.br"},
     {id:3,name:"João",email:"joao@joao.com.br"},
-]
+];
+
 module.exports = {
     Query: {
         users: () => {
@@ -22,8 +23,8 @@ module.exports = {
         createUser: (_,{name, email}) => {
             return {id:999999,name,email};
         },
-        editUser: (_,{}) => {
-            
+        updateUser: (_,{id, name, email}) => {
+            return {id,name,email};
         }
-    }
+    },
 }
